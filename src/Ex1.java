c6a7aca3a60af5975dd659761b4ad0b49c92693f
gitteolhub <ht2001@naver.com>
2024-08-04 14:42:32 +0900

    error test

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,7 +1,7 @@
 public class Ex1 {
     public static void main(String[] args){
-        int kor = 95;
-        int eng = 90;
+        int kor = 100;
+        int eng = 95;
         int math = 88;
         int sum = kor + eng + math;
         float avg = Math.round((float)sum / 3 * 1e3) / 1000f; //소수점 'n'자리까지 반올림 >> 1e'n'F (나중에 공부해서 규칙성 함수화 가능)
